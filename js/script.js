371fbc1675f7ed50edf6de6cf069034d356bbe5e
Владислав Гейна <ipz215_gvs@student.ztu.edu.ua>
2022-07-20 19:07:54 +0300

    Added .gitignore add modified js file

--- js/script.js
+++ js/script.js
@@ -19,3 +19,20 @@ if(we) {
       },
     });
 }
+let animItems  =document.querySelectorAll("._anim-items");
+if(animItems.length > 0) {
+  function animOnScroll(params) {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return {top:rect.top + scrollTop, left : rect.left + scrollLeft}
+  }
+}
